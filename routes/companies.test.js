879e3e2d3f669b8f339c21044ab92f53921d86d4
Ryan <ryjen.pears@gmail.com>
2022-04-05 15:51:56 +0200

    tests for route added

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -96,15 +96,48 @@ describe("GET /companies", function () {
     });
   });
 
-  test("fails: test next() handler", async function () {
-    // there's no normal failure event which will cause this route to fail ---
-    // thus making it hard to test that the error-handler works with it. This
-    // should cause an error, all right :)
-    await db.query("DROP TABLE companies CASCADE");
+  test("works: filtering", async () => {
     const resp = await request(app)
-        .get("/companies")
-        .set("authorization", `Bearer ${u1Token}`);
-    expect(resp.statusCode).toEqual(500);
+      .get("/companies")
+      .query({ minEmployees: 3 });
+    
+    expect(resp.body).toEqual({
+      companies: [
+        {
+          handle: "c3",
+          name: "C3",
+          description: "Desc3",
+          numEmployees: 3,
+          logoUrl: "http://c3.img",
+        },        
+      ],
+    });
+  });
+
+  test("works: filtering on all filters", async () => {
+    const resp = await request(app)
+      .get("/companies")
+      .query({ minEmployees: 2, maxEmployees: 3, name: "3" });
+    
+    expect(resp.body).toEqual({
+      companies: [
+        {
+          handle: "c3",
+          name: "C3",
+          description: "Desc3",
+          numEmployees: 3,
+          logoUrl: "http://c3.img",
+        },
+      ],
+    });
+  });
+
+  test("bad request if invalid filter key", async () => {
+    const resp = await request(app)
+      .get("/companies")
+      .query({ minEmployees: 2, nothing: "nothing" });
+    
+    expect(resp.statusCode).toEqual(400);
   });
 });
 
